756e6e68ee0cbf7a9f326534f0252419b9c9f503
SNIR_admin <SNIR_admin@10-SNIR-14.560016n.ac-rennes.fr>
2023-09-14 11:00:26 +0200

    Conducteur.h completé

--- Conducteur.h
+++ Conducteur.h
@@ -1,1 +1,36 @@
-#pragma once
+#ifndef CONDUCTEUR_H
+#define CONDUCTEUR_H
+#include <iostream>
+#include <string>
+class Moto;
+
+class Conducteur
+{
+
+private:
+
+    std::string nomConducteur;
+
+    std::string prenomConducteur;
+
+    int anneNaissance;
+
+
+    Moto* mesMotos[10];
+
+protected:
+
+
+public:
+
+    std::string getNom();
+    std::string getPrenom();
+    std::string getAnneNaissance();
+
+    void afficheMotos();
+    void addMoto(Moto* newMoto);
+    void rmMoto(Moto* rmMoto);
+
+};
+
+#endif
