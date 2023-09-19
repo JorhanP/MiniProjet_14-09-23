707f72fea8c7d0f02f3819941e2f2a339639a5c0
SNIR_admin <SNIR_admin@10-SNIR-14.560016n.ac-rennes.fr>
2023-09-19 16:42:21 +0200

    Main continuation

--- Main_J.cpp
+++ Main_J.cpp
@@ -4,10 +4,13 @@
 #include <iostream>
 #include"Conducteur.h"
 #include"Moteur.h"
+#include"Moto.h"
+#include"Electrique.h"
+#include"Thermique.h"
 using namespace std;
 
 
-int annee;
+int annee, poid, newpuissance;
 string nom, prenom;
 
 
@@ -20,16 +23,30 @@ int main() {
 	cout << "Prenom" << endl;
 	cin >> prenom;
 
-	Conducteur nouveauConducteur(nom,prenom,annee);
+	Conducteur nouveauConducteur(nom, prenom, annee);
 
 	cout << "Le nom du conducteur est " << nouveauConducteur.getNom() << "\n";
 	cout << "Le prenom du conducteur est " << nouveauConducteur.getPrenom() << "\n";
 	cout << "L'age du conducteur est " << nouveauConducteur.getAnneNaissance() << "\n";
 
-	Moto nouvelleMoto();
 
 
+	Moteur Moteur1;
 
+	cout << "Puissance du moteur ?" << endl;
+	cin >> newpuissance;
+
+	Moteur1.setPuissance(newpuissance);
+	cout << "Le puisssance du moto est " << Moteur1.getPuissance() << "\n";
+
+
+	Moto Moto1;
+
+	cout << "Poid de la moto ?" << endl;
+	cin >> poid;
+
+	Moto1.setPoids(poid);
+	cout << "Le poid de la moto est " << Moto1.getPoids() << "\n";
 
 
 }
